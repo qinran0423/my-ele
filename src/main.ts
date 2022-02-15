5aa4a4912b640772da25b89bd8f80746117a192c
qinran0423 <1156107187@qq.com>
2022-02-15 17:21:00 +0800

    feat: add button test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElContainer from './components/container'
+import ElButton from './components/button';
 
 const app = createApp(App)
-app.use(ElContainer).mount('#app')
+
+app.config.globalProperties.$ELEMENT = {
+  size: 'large'
+}
+
+app.use(ElContainer)
+  .use(ElButton).mount('#app')
