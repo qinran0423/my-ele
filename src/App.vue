d4200ad9567fa8faa9a0dde949e7ef648cecc4e5
qinran0423 <1156107187@qq.com>
2022-02-15 17:21:35 +0800

    fix: eslint App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <script setup lang="ts"></script>
 
 <template>
-  <el-button type="primary"> 按钮 </el-button>
-  <el-button type="success"> 按钮 </el-button>
+  <el-button type="primary">
+    按钮
+  </el-button>
+  <el-button type="success">
+    按钮
+  </el-button>
   <el-button>按钮</el-button>
-  <el-button size="small"> 按钮 </el-button>
+  <el-button size="small">
+    按钮
+  </el-button>
 
   <el-container>
     <el-header>Header</el-header>
     <el-main>Main</el-main>
     <el-footer>Footer</el-footer>
   </el-container>
-  <hr />
+  <hr>
 
   <el-container>
     <el-header>Header</el-header>
     <el-container>
-      <el-aside width="200px"> Aside </el-aside>
+      <el-aside width="200px">
+        Aside
+      </el-aside>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
-  <hr />
+  <hr>
   <el-container>
-    <el-aside width="200px"> Aside </el-aside>
+    <el-aside width="200px">
+      Aside
+    </el-aside>
     <el-container>
       <el-header>Header</el-header>
       <el-main>Main</el-main>
